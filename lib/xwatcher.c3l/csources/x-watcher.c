4aaee54eef0cc7a6ebe637b48bd9e672c31639ef
Elusive239 <56003608+Elusive239@users.noreply.github.com>
2025-06-29 14:33:27 -0400

    Initial commit

---
+++ lib/xwatcher.c3l/csources/x-watcher.c
@@ -0,0 +1,8 @@
+#include "array.h"
+#include "x-watcher.h"
+
+// x_watcher *xWatcher_create(void);
+// bool xWatcher_appendFile(x_watcher *watcher, xWatcher_reference *reference)
+// bool xWatcher_appendDir(x_watcher *watcher, xWatcher_reference *reference)
+// bool xWatcher_start(x_watcher *watcher);
+// void xWatcher_destroy(x_watcher *watcher);
